bnordgren/nixpkgs

commit 1cdc52225552af270d0c08e09fa386acafcc7961

* Merged the stdenv-updates branch.  Enjoy :-)
git-svn-id: https://nixos.org/repos/nix/nixpkgs/trunk@15324 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/tools/text/namazu/default.nix
@@ -11,10 +11,11 @@ stdenv.mkDerivation rec {
   buildInputs = [ perl ];
 
   # First install the `File::MMagic' Perl module.
+  # !!! this shouldn't refer to Perl 5.10.0!
   preConfigure = ''
     ( cd File-MMagic &&				\
       perl Makefile.PL				\
-        LIB="$out/lib/site_perl/5.10.0"		\
+        LIB="$out/lib/perl5/site_perl/5.10.0"	\
         INSTALLSITEMAN3DIR="$out/man" &&	\
       make && make install )
     export PERL5LIB="$out/lib/site_perl/5.10.0:$PERL5LIB"

pkgs/applications/audio/bmp-plugins/wma/default.nix
@@ -2,10 +2,18 @@
 
 stdenv.mkDerivation {
   name = "bmp-plugin-wma-1.0.5";
-  builder = ./builder.sh;
+  
   src = fetchurl {
     url = http://mcmcc.bat.ru/xmms-wma/xmms-wma-1.0.5.tar.bz2;
     md5 = "5d62a0f969617aeb40096362c7a8a506";
   };
+  
   buildInputs = [pkgconfig bmp];
+
+  buildFlags = "-f Makefile.bmp";
+  
+  installPhase = ''
+    ensureDir "$out/lib/bmp/Input"
+    cp libwma.so "$out/lib/bmp/Input"
+  '';
 }

pkgs/applications/audio/flac/default.nix
@@ -9,6 +9,14 @@ stdenv.mkDerivation rec {
   };
 
   buildInputs = [libogg];
+
+  patches = [
+    # Fix for building on GCC 4.3.
+    (fetchurl {
+      url = "http://sources.gentoo.org/viewcvs.py/*checkout*/gentoo-x86/media-libs/flac/files/flac-1.2.1-gcc-4.3-includes.patch?rev=1.1";
+      sha256 = "1m6ql5vyjb2jlp5qiqp6w0drq1m6x6y3i1dnl5ywywl3zd36k0mr";
+    })
+  ];
   
   meta = {
     homepage = http://flac.sourceforge.net;

pkgs/applications/audio/pavucontrol/default.nix
@@ -1,0 +1,30 @@
+{ fetchurl, stdenv, pkgconfig, pulseaudio, gtkmm, libsigcxx
+, libglademm, libcanberra, intltool, gettext }:
+
+stdenv.mkDerivation rec {
+  name = "pavucontrol-0.9.7";
+
+  src = fetchurl {
+    url = "http://0pointer.de/lennart/projects/pavucontrol/${name}.tar.gz";
+    sha256 = "1a1v06hbl1j78ryqy5aiccg6w5hf1yzday2b9h31kx7vr42ir1w0";
+  };
+
+  buildInputs = [ pkgconfig pulseaudio gtkmm libsigcxx libglademm libcanberra
+    intltool gettext ];
+
+  configureFlags = "--disable-lynx";
+
+  meta = {
+    description = "PulseAudio Volume Control";
+
+    longDescription = ''
+      PulseAudio Volume Control (pavucontrol) provides a GTK+
+      graphical user interface to connect to a PulseAudio server and
+      easily control the volume of all clients, sinks, etc.
+    '';
+
+    homepage = http://0pointer.de/lennart/projects/pavucontrol/;
+
+    license = "GPLv2+";
+  };
+}

pkgs/applications/graphics/inkscape/default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
     sha256 = "0flrjqa68vnnn8lrhj86xpa6h2cyzrvjy6873v9id092f86ix1li";
   };
 
-  patches = [ ./configure-python-libs.patch ./libpng-setjmp.patch ]; 
+  patches = [ ./configure-python-libs.patch ./libpng-setjmp.patch ./gtk-clist.patch ]; 
 
   propagatedBuildInputs = [
     # Python is used at run-time to execute scripts, e.g., those from
@@ -23,6 +23,14 @@ stdenv.mkDerivation rec {
   ];
 
   configureFlags = "--with-python";
+
+  # Fix compilation on glibc 2.9 by adding missing string header
+  preConfigure = ''
+    echo "#include <string.h>" > tmp.cpp
+    cat tmp.cpp src/dom/io/uristream.cpp > src/dom/io/uristream.cpp.new
+    rm tmp.cpp
+    mv src/dom/io/uristream.cpp.new src/dom/io/uristream.cpp
+  '';
 
   preBuild = ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -I./extension/script"

pkgs/applications/misc/cdrtools/default.nix
@@ -2,7 +2,9 @@
 
 stdenv.mkDerivation {
   name = "cdrtools-2.01";
-  builder = ./builder.sh;
+  
+  configurePhase = "prefix=$out";  
+  
   #hack, I'm getting "chown: invalid user: `bin" error, so replace chown by a nop dummy script
   preInstall = ''
     mkdir "$TMP/bin"
@@ -12,10 +14,12 @@ stdenv.mkDerivation {
       PATH="$TMP/bin:$PATH"
     done
   '';
+  
   src = fetchurl {
     url = ftp://ftp.berlios.de/pub/cdrecord/cdrtools-2.01.tar.bz2;
     md5 = "d44a81460e97ae02931c31188fe8d3fd";
   };
+  
   patches = [./cdrtools-2.01-install.patch];
 
   meta = {

pkgs/applications/misc/evince/default.nix
@@ -1,26 +1,26 @@
-{ fetchurl, stdenv, perl, perlXMLParser, gettext
+{ fetchurl, stdenv, perl, perlXMLParser, gettext, intltool
 , pkgconfig, glib, gtk, gnomedocutils, gnomeicontheme
 , libgnome, libgnomeui, scrollkeeper, libxslt
 , libglade, dbus, dbus_glib
 , poppler, libspectre, djvulibre, shared_mime_info
-, makeWrapper }:
+, makeWrapper, which }:
 
 stdenv.mkDerivation rec {
-  name = "evince-2.23.4";
+  name = "evince-2.26.0";
 
   src = fetchurl {
-    url = "http://ftp.gnome.org/pub/GNOME/sources/evince/2.23/${name}.tar.bz2";
-    sha256 = "0svdpx9vkh99vbxsfk5ppi6xl0wqvqsrii7m26irinf0bkfyc964";
+    url = "http://ftp.gnome.org/pub/GNOME/sources/evince/2.26/${name}.tar.bz2";
+    sha256 = "1wsl5vdrj0829wq223dryq5p7izgzsz6mfl4igix7b5wga42zff1";
   };
 
   buildInputs = [
-    perl perlXMLParser gettext
+    perl perlXMLParser gettext intltool
     pkgconfig glib gtk gnomedocutils gnomeicontheme
     libgnome libgnomeui libglade scrollkeeper
     libxslt  # for `xsltproc'
     dbus dbus_glib
     poppler libspectre djvulibre
-    makeWrapper
+    makeWrapper which
   ];
 
   configureFlags = "--with-libgnome --enable-dbus --enable-pixbuf "

pkgs/applications/misc/gphoto2/default.nix
@@ -3,11 +3,11 @@
 }:
 
 stdenv.mkDerivation rec {
-  name = "gphoto2-2.4.2";
+  name = "gphoto2-2.4.5";
   
   src = fetchurl {
     url = "mirror://sourceforge/gphoto/${name}.tar.bz2";
-    sha256 = "0wna84rli816d830hirdv3ficr3q16zs471l725rgsdvc4pqrvy9";
+    sha256 = "0bjbgz9n7ijf8167i1bm7q3pg366axgx5zydck13d2znhd30x069";
   };
   
   buildInputs = [pkgconfig libgphoto2 libexif popt gettext libjpeg readline libtool];

commit 117edb7eb05932bddd97268e0ea8be97614f0438

Namazu: Fix builds, broken in r15324.
git-svn-id: https://nixos.org/repos/nix/nixpkgs/trunk@16069 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/tools/text/namazu/default.nix
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
         LIB="$out/lib/perl5/site_perl/5.10.0"	\
         INSTALLSITEMAN3DIR="$out/man" &&	\
       make && make install )
-    export PERL5LIB="$out/lib/site_perl/5.10.0:$PERL5LIB"
+    export PERL5LIB="$out/lib/perl5/site_perl/5.10.0:$PERL5LIB"
   '';
 
   doCheck = true;