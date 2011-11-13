bnordgren/nixpkgs

commit f4c6b6185d2479c208fa9958af0c3940c1b5b754

Do not apply Hurd-specific fixes for simple cross-Linux. Unfortunately, still no luck with my ARM device
git-svn-id: https://nixos.org/repos/nix/nixpkgs/trunk@30410 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/development/compilers/gcc-4.5/default.nix
@@ -147,7 +147,6 @@ stdenv.mkDerivation ({
 
   postPatch =
     if (stdenv.system == "i586-pc-gnu"
-        || (libcCross != null)                 # e.g., building `gcc.hostDrv'
         || (cross != null && cross.config == "i586-pc-gnu"
             && libcCross != null))
     then

commit 080cc003d4882ba35a25cf1c4798a03c87cf98f7

GCC 4.5: Fix `gcc45.hostDrv' for GNU.
This fixes a regression introduced in r30410 ("Do not apply
Hurd-specific fixes for simple cross-Linux. Unfortunately, still no luck
with my ARM device").

git-svn-id: https://nixos.org/repos/nix/nixpkgs/trunk@30412 70bd8c7a-acb8-0310-9f0d-9cc1c95dcdbb

pkgs/development/compilers/gcc-4.5/default.nix
@@ -147,6 +147,8 @@ stdenv.mkDerivation ({
 
   postPatch =
     if (stdenv.system == "i586-pc-gnu"
+        || (libcCross != null                  # e.g., building `gcc.hostDrv'
+            && libcCross.crossConfig == "i586-pc-gnu")
         || (cross != null && cross.config == "i586-pc-gnu"
             && libcCross != null))
     then