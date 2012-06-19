8ed401fd2f1cb5e548b1a9b9fdaee736991501d4
Bryce Nordgren <bnordgren@gmail.com>
2012-06-19 23:31:27 +0200

    Compiles geonetwork with maven build

--- pkgs/servers/http/geonetwork/builder.sh
+++ pkgs/servers/http/geonetwork/builder.sh
@@ -1,5 +1,17 @@
 source $stdenv/setup
 
+export LANG="en_US.UTF-8"
+export LOCALE_ARCHIVE=$glibcLocales/lib/locale/locale-archive
+export M2_REPO=$TMPDIR/repository
 
-cd $src
-mvn clean install
+echo "XXXXX>>>>> Maven repository: " $M2_REPO
+
+mkdir -p $TMPDIR
+cp -r $src $TMPDIR/src
+chmod -R u+w $TMPDIR/src
+cd $TMPDIR/src
+mvn -debug -Dmaven.repo.local=$M2_REPO install > $TMPDIR/build.log
+
+mkdir -p $out
+cp -r $TMPDIR/repository $out
+
